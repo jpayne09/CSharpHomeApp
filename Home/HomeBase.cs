41072877a4ab9f1aa65b4dc4fc656431665ad1ce
John Payne <jlpayne09@live.com>
2022-02-22 10:16:39 -0500

    Add files via upload

---
+++ Home/HomeBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Home
+{
+    public abstract class HomeBase
+    {
+        private string HomeType { get; set; }
+        private string MaintenanceFee { get; set; }
+        private string HomeInsurance { get; set; }
+
+
+
+        public abstract void getHomeInsuranceFee(double inFee);
+        public abstract void getMaintenanceFee(double homeFee);
+    }
+}
